67ebb7242fab60a5642188c767ba19c248643b00
Racer <reaganjoseph26@yahoo.com>
2020-12-28 17:48:18 -0500

    first questions rendered with html

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const inquirer = require('inquirer')
+const Manager = require('./lib/Manager')
+const generateHTML = require('./utils/generate-html')
+
  
 const promptUser = () => {
     return inquirer
     .prompt([
         {
-            type: list,
-            name: 'name',
+            type: 'list',
+            name: 'employeeType',
             message: 'Please select a type of employee. (Required)',
-            choices: ['Engineer', 'Intern', 'Manager'],
+            choices: ['Manager', 'Engineer', 'Intern'],
             validate: nameInput => {
                 if(nameInput) {
                     return true
@@ -16,7 +19,35 @@ const promptUser = () => {
                     return false;
                 } 
             }
-         },
+        }
+       
+        // {
+        //     type: 'input',
+        //     name: 'Manager',
+        //     message: ''
+        // }
+    
+
          
-])
+    ])
 }
+    
+// promptUser()
+//     .then(generateHTML)
+
+function init() {
+    promptUser().then((userAnswers) => {
+        const employeeType = userAnswers.employeeType
+        if(employeeType === 'Manager') {
+            const newManager = new Manager('John,', 1, 'test@yahoo', 24)
+            generateHTML(newManager)
+            
+        }
+        //check for type of employee\
+        //function if employee is engineer, ask engineer. questions for engineer
+
+        // generateHTML(userAnswers)
+    })
+}
+
+init();
---
+++ utils/generate-html.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+// const employee = require('../lib/Employee')
+
+function renderEmployee(employee) {
+    let employeeContent
+    console.log(employee)
+    if (employee === 'Manager') {
+      employeeContent = 
+      `<div class="employee-card">
+        <h3>${employee}</h3>
+        <p>John Smith</p>
+        <p>JohnSmith@yahoo.com</p>
+      </div>` 
+    } else {}
+
+    return employeeContent;
+};
+
+function generateHTML(data) {
+    console.log(data)
+    let content =  `
+
+   ${renderEmployee(data.employee)}
+
+`;
+writeToFile('./dist/index.html', content);
+}
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', data, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+  
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'Success!'
+            })
+        })
+    })
+  }
+  
+
+module.exports = generateHTML
