5c34fd98135d0eec1f55addcf679e34adc61eb4b
Racer <reaganjoseph26@yahoo.com>
2020-12-30 14:49:18 -0500

    icons added

--- utils/generate-html.js
+++ utils/generate-html.js
@@ -10,7 +10,7 @@ function renderEmployee(employeeData) {
       `<div class="card" style="width: 18rem;">
         <div class="card-body">
         <h3>${employeeData.getName()}</h3>
-        <h4>${employeeData.getRole()}</h4>
+        <h4><i class="fas fa-mug-hot"></i> ${employeeData.getRole()}</h4>
        </div>
        <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${employeeData.getId()}</li>
@@ -23,7 +23,7 @@ function renderEmployee(employeeData) {
       `<div class="card" style="width: 18rem;">
         <div class="card-body">
         <h3>${employeeData.getName()}</h3>
-        <h4>${employeeData.getRole()}</h4>
+        <h4><i class="fas fa-glasses"></i> ${employeeData.getRole()}</h4>
         </div>
         <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${employeeData.getId()}</li>
@@ -36,7 +36,7 @@ function renderEmployee(employeeData) {
       `<div class="card" style="width: 18rem;">
       <div class="card-body">
       <h3>${employeeData.getName()}</h3>
-      <h4>${employeeData.getRole()}</h4>
+      <h4><i class="fas fa-user-graduate"></i> ${employeeData.getRole()}</h4>
       </div>
       <ul class="list-group list-group-flush">
       <li class="list-group-item">ID: ${employeeData.getId()}</li>
@@ -61,6 +61,7 @@ function generateHTML(data) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Team Profiles</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous"/>
     <link rel="stylesheet" href="../src/stylesheet.css">
   </head>
 
