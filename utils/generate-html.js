1d350de21de1a8eee81f59aa7d37db3c4e6ae8b4
Racer <reaganjoseph26@yahoo.com>
2020-12-30 11:57:09 -0500

    card css added to all employee types

--- utils/generate-html.js
+++ utils/generate-html.js
@@ -20,21 +20,29 @@ function renderEmployee(employeeData) {
       </div>` 
     } else if  (employeeData.getRole() === 'Engineer') {
       employeeContent = 
-      `<div class="employee-card">
+      `<div class="card" style="width: 18rem;">
+        <div class="card-body>
         <h3>${employeeData.getName()}</h3>
         <h4>${employeeData.getRole()}</h4>
-        <p> ID: ${employeeData.getId()}</p>
-        <p><a href="mailto:${employeeData.getEmail()}">Email: ${employeeData.getEmail()}</a></p>
-        <a href="https://github.com/${employeeData.getGithub()}">Gitub: ${employeeData.getGithub()}</a>
+        </div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${employeeData.getId()}</li>
+        <li class="list-group-item"><a href="mailto:${employeeData.getEmail()}">Email: ${employeeData.getEmail()}</a}</li>
+        <li class="list-group-item"><a href="https://github.com/${employeeData.getGithub()}">Gitub: ${employeeData.getGithub()}</a></li>
+        </ul>
       </div>` 
     } else if (employeeData.getRole() === 'Intern') {
       employeeContent = 
-      `<div class="employee-card">
-        <h3>${employeeData.getName()}</h3>
-        <h4>${employeeData.getRole()}</h4>
-        <p> ID: ${employeeData.getId()}</p>
-        <p><a href="mailto:${employeeData.getEmail()}">Email: ${employeeData.getEmail()}</a></p>
-        <p>School: ${employeeData.getSchool()}</p>
+      `<div class="card" style="width: 18rem;">
+      <div class="card-body>
+      <h3>${employeeData.getName()}</h3>
+      <h4>${employeeData.getRole()}</h4>
+      </div>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${employeeData.getId()}</li>
+      <li class="list-group-item"><a href="mailto:${employeeData.getEmail()}">Email: ${employeeData.getEmail()}</a}</li>
+      <li class="list-group-item">${employeeData.getSchool()}</li>
+      </ul>
       </div>` 
     }
 
@@ -53,7 +61,7 @@ function generateHTML(data) {
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Team Profiles</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-      <link rel="stylesheet" href="./src/stylesheet.css">
+      <link rel="stylesheet" href="../src/stylesheet.css">
     </head>
 
     <h1>My Team</h1>
